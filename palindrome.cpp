2df8074c5f986fd56b43be437203116bf9d81ef6
U-BSD\523158 <523158@SHS-84W2NW3.beaverton.k12.or.us>
2024-09-10 10:06:29 -0700

    9/10 changes

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 #include <cstring>
+#include <cctype>
 
 using namespace std;
 
 int main()
 {
-  char str1[80];
-  cin.get(str1, 80); 
-  char str2[80];
-  for (int i = 0; i < 80; i++)
+  char str[80];
+  cin.get(str, 80); 
+  char cleaned[80];
+  int cleanedIndex = 0;
+  char reversed[80];
+  for (int i = 0; str[i] != '\0'; i++)
   {
-    if(str[i] != '\0')
+    if(isalnum(str[i]))
       {
-	str2[count]=str1[i];
-	count++;
-      }
-     
+	if(isupper(str[i]))
+	  {
+	    tolower(str[i]);
+	  }
+	cleaned[cleanedIndex] = str[i];
+	cleanedIndex++;
+      } 
   }
+
+  for(int i = 0; i < strlen(cleaned); i--)
+    {
+      reversed[i] = cleaned[strlen(cleaned) - i];
+    }
+
+  if(strcmp(cleaned, reversed) == 0)
+    {
+      cout << "Palindrome" << endl;
+    }
+
+  else
+    {
+      cout << "Not a palindrome" << endl;
+    }
+
+  return 0; 
 }
