49e30febc1c70e1a40a63b4311e497664b6b5e31
Emma <emma.bendersky06@gmail.com>
2025-02-08 00:09:11 -0800

    fourth commit

--- hashTable/HashTable.h
+++ hashTable/HashTable.h
@@ -4,29 +4,30 @@
 
 using namespace std;
 
+//Stucture to represent a student
 struct Student
 {
   int ID;
-  char firstName[80];
-  char lastName[80];
+  char* firstName;
+  char* lastName;
   float GPA;
-  Student* next;
+  Student* next; //Pointer to next student
 };
   
 class HashTable
 {
 private:
-  Student** table;
+  Student** table; //Pointer to array of student pointers (table)
   int tableSize;
-  int IDcounter;
+  int IDcounter; //Counter for generating unique student IDs
 
 public:
-  HashTable(int);
-  ~HashTable();
-  int hashFunction(int);
-  void rehash();
-  void addStudent(char, char, float);
-  void printStudents();
-  void deleteStudent(int);
+  HashTable(int); //Constuctor: Initializes the hash table wioth a given size 
+  ~HashTable(); //Destructor: Deallocates the memory for the has table and students
+  int hashFunction(int); //Hash finction: Computes a has value for a given student ID
+  void rehash(); //Rehashing: Resizes the hash table anf redistributes students in case of too many collisions 
+  void addStudent(char*, char*, float); //Function do add a student to the hash table
+  void printStudents(); //Function to print the details of all students in the hash table
+  void deleteStudent(int); //Function to delete a student from the has tabple by their ID
 };
 #endif
